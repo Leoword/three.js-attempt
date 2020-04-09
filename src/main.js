0ef6546f535b8352e5c65292115598e0f2bf6eaf
leoword <1265249619@qq.com>
2020-04-09 13:29:32 +0800

    [INIT] the programe base code

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import * as THREE from 'three/build/three.min';
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
